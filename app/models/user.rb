9e647ca35153a1ca35e59d07cce8bd19497e8dfc
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-21 19:31:19 +0700

    user_sign_up_with_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable, :rememberable,
   devise :database_authenticatable, :registerable, :validatable
+
+  validates :name, presence: true
 end
