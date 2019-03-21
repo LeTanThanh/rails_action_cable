30fbc76e04c3ef4ea36d62eddd0e80756b1700ec
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-21 19:14:15 +0700

    add_flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def alert_class type
+    case type.to_s
+    when "notice"
+      "success"
+    when "alert"
+      "danger"
+    else
+      type
+    end
+  end
 end
