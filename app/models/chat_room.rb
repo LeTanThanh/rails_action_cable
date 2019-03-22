27b76b3f9b562c69c5b7159547d9210fd45ba29e
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-22 10:25:05 +0700

    add_model_messages

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,5 +1,6 @@
 class ChatRoom < ApplicationRecord
   belongs_to :user
+  has_many :messages, dependent: :destroe
 
   validates :title, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :chat_rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
 end
