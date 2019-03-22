e381b3ee2f1ef33858e9279a09a7b193218a4875
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-22 11:37:37 +0700

    seed_messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 # seed users
+User.create name: "TanThanhLe", email: "tanthanhcr95@gmail.com",
+  password: "12345678", password_confirmation: "12345678"
+
 10.times do
   name = FFaker::Name.name
   email = name.split(" ").map(&:downcase).join("_") + "@gmail.com"
@@ -12,4 +15,13 @@ User.all.sample(10).each do |user|
   2.times do
     user.chat_rooms.create title: FFaker::Lorem.phrase
   end
+end
+
+# seed messages
+ChatRoom.all.sample(10).each do |chat_room|
+  User.all.sample(10).each do |user|
+    user.messages.create chat_room: chat_room, body: FFaker::Lorem.phrase
+  end
+
+  User.first.messages.create chat_room: chat_room, body: FFaker::Lorem.phrase
 end
