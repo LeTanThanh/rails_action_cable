24f56f27f7fd262acc2be12bbcffc7175c691151
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-22 19:16:42 +0700

    render_correct_message_side

--- app/assets/javascripts/channels/chat_rooms.js
+++ app/assets/javascripts/channels/chat_rooms.js
@@ -1,21 +1,26 @@
 (function() {
   jQuery(document).on('turbolinks:load', function() {
-    var messages;
-    messages = $('#js-container-messages');
+    var messages = $('#js-container-messages');
 
-    if ($('#js-container-messages').length > 0) {
+    if (messages.length > 0) {
+      var chat_room_id = messages.data('chat-room-id');
+      var current_user_id = messages.data('current-user-id');
+
       App.client_connection = App.cable.subscriptions.create({
         channel: 'ChatRoomsChannel',
-        chat_room_id: messages.data('chat-room-id')
+        chat_room_id: chat_room_id
       }, {
         connected: function() {
         },
         disconnected: function() {
         },
         received: function(data) {
-          messages.append(data['message']);
+          if(current_user_id == data['user_message_id']) {
+            messages.append(data['user_message']);
+          } else {
+            messages.append(data['other_user_message']);
+          }
         },
-
         send_message_to_server_channel: function(message, chat_room_id) {
           this.perform('receive_message_from_client', {
             message: message,
@@ -25,9 +30,8 @@
       });
 
       $('#js-form-send-message').submit(function(e) {
-        var $this, textarea;
-        $this = $(this);
-        textarea = $this.find('#js-textarea-message');
+        var $this = $(this);
+        var textarea = $this.find('#js-textarea-message');
 
         if ($.trim(textarea.val()).length > 1) {
           App.client_connection.send_message_to_server_channel(textarea.val(), messages.data('chat-room-id'));
--- app/jobs/action_cable_broadcast_message_job.rb
+++ app/jobs/action_cable_broadcast_message_job.rb
@@ -3,12 +3,18 @@ class ActionCableBroadcastMessageJob < ApplicationJob
 
   def perform message
     ActionCable.server.broadcast "chat_rooms_#{message.chat_room_id}_channel",
-      message: render_message(message)
+      user_message_id: message.user_id,
+      user_message: render_user_message(message),
+      other_user_message: render_other_user_message(message)
   end
 
   private
 
-  def render_message message
+  def render_user_message message
     ApplicationController.render partial: "chat_rooms/user_message", locals: {message: message, user: message.user}
+  end
+
+  def render_other_user_message message
+    ApplicationController.render partial: "chat_rooms/other_user_message", locals: {message: message, user: message.user}
   end
 end
